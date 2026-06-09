78ecf099eb66ecc652248e8fa7e50a3e2688186b
prradnya24 <pradnyalate27@gmail.com>
2026-06-09 12:24:32 +0530

    add import for uploadDocument and getDocuments from documentController

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const protect = require("./middleware/authMiddleware");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const documentRoutes = require("./routes/documentRoutes");
+const {
+  uploadDocument,
+  getDocuments
+} = require("../controllers/documentController");
 const app = express();
 
 connectDB();
