c06d9208d2c13b1a72d160a41c2fb68aa3fb6c23
prradnya24 <pradnyalate27@gmail.com>
2026-06-09 11:33:15 +0530

    added routes in server file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -11,6 +13,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("AI Knowledge Base API Running");
